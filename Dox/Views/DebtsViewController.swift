6f0c2829bf90a3ad6a8552849a7f1d308171e0fa
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-25 20:48:46 -0300

    added table test to vc

--- Dox/Views/DebtsViewController.swift
+++ Dox/Views/DebtsViewController.swift
@@ -9,8 +9,46 @@
 import UIKit
 
 class DebtsViewController: UIViewController {
-
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        return tableView
+    }()
+    
+    var tableViewItems: [String] = ["Aaaa", "Bbbbb", "Cccc"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.AppColors.darkGray
+        tableView.register(DebtCell.self, forCellReuseIdentifier: "cell")
+        
+        NSLayoutConstraint.activate([
+            tableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7),
+            tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+    }
+}
+
+extension DebtsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? DebtCell
+            else { return UITableViewCell() }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 128
     }
 }
