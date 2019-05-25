17275885dd3e00ba368e9caf63b86242ba07200d
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-25 12:18:07 -0300

    fixed appColors

--- Dox/UIColor+AppColors.swift
+++ Dox/UIColor+AppColors.swift
@@ -12,7 +12,8 @@ extension UIColor {
     struct AppColors {
         static let orange = #colorLiteral(red: 0.9411764706, green: 0.6549019608, blue: 0.4431372549, alpha: 1)
         static let darkGray = #colorLiteral(red: 0.2274509804, green: 0.2274509804, blue: 0.2274509804, alpha: 1)
-        static let gray = #colorLiteral(red: 0.2705882353, green: 0.2705882353, blue: 0.2705882353, alpha: 1)
+        static let mediumGray = #colorLiteral(red: 0.2705882353, green: 0.2705882353, blue: 0.2705882353, alpha: 1)
+        static let gray = #colorLiteral(red: 0.4862745098, green: 0.4862745098, blue: 0.4862745098, alpha: 1)
         static let lightGray = #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
         static let darkShadow = #colorLiteral(red: 0.1921568627, green: 0.1882352941, blue: 0.1882352941, alpha: 1)
         static let lighShadow = #colorLiteral(red: 0.2705882353, green: 0.2705882353, blue: 0.2705882353, alpha: 1)
