0e7f50c980a8a4a0a60ef53ced60c5830f872b3d
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 12:39:53 -0300

    added attributed protocol

---
+++ Dox/Views/NewDebtVC/Components/CardTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CardTextField.swift
+//  Dox
+//
+//  Created by Bia Plutarco on 26/05/19.
+//  Copyright © 2019 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class CardTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
