f89a3bf8b9cebaf6228d0cecc2219847337d59da
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 00:22:16 -0300

    fixed mocklabel config text func

--- Dox/Ultis/Label/MockLabel.swift
+++ Dox/Ultis/Label/MockLabel.swift
@@ -14,12 +14,15 @@ enum MockLabelText: String {
     case reason = "Reason"
     case type = "Type"
     case save = "Save"
+    case debts = "Debts"
+    case newDebt = "New Debt"
 }
 
 enum MockLabelType {
     case cardDebt
     case viewNewDebt
     case button
+    case title
 }
 
 class MockLabel: UILabel {
@@ -35,20 +38,30 @@ class MockLabel: UILabel {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+}
+
+extension MockLabel {
     private func configLabel(text: String, type: MockLabelType) {
         switch type {
         case .cardDebt:
-            self.attributedText = configAtributedText(text: text, fontSize: 12, color: UIColor.AppColors.gray)
+            self.attributedText = configText(text, size: 12, color: UIColor.AppColors.gray, isBold: false)
         case .button:
-            self.attributedText = configAtributedText(text: text, fontSize: 18, color: UIColor.AppColors.lightGray)
+            self.attributedText = configText(text, size: 18, color: UIColor.AppColors.lightGray, isBold: true)
+        case .title:
+            self.attributedText = configText(text, size: 34, color: UIColor.AppColors.lightGray, isBold: true)
         default:
-            self.attributedText = configAtributedText(text: text, fontSize: 20, color: UIColor.AppColors.darkGray)
+            self.attributedText = configText(text, size: 20, color: UIColor.AppColors.darkGray, isBold: false)
         }
     }
     
-    private func configAtributedText(text: String, fontSize: CGFloat, color: UIColor) -> NSMutableAttributedString {
-        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize),
+    private func configText(_ text: String, size: CGFloat, color: UIColor, isBold: Bool) -> NSMutableAttributedString {
+        var font = UIFont()
+        if isBold == true {
+            font = UIFont.boldSystemFont(ofSize: size)
+        } else {
+            font = UIFont.systemFont(ofSize: size)
+        }
+        let attributes = [NSAttributedString.Key.font: font,
                           NSAttributedString.Key.foregroundColor: color]
         return NSMutableAttributedString(string: text, attributes: attributes)
     }
