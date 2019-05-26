a3459fa85b14a3676bfbb82298b2015bf9ca2ebb
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 12:40:48 -0300

    git added save button

--- Dox/Views/NewDebtVC/Components/RectangleButton.swift
+++ Dox/Views/NewDebtVC/Components/RectangleButton.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class RectangleButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    convenience init(title: String) {
+        self.init(frame: CGRect.zero)
+        configButton(title: title)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension RectangleButton: ShadowProtocol {
+    private func configButton(title: String) {
+        self.configShadowIn(view: self, isDark: false)
+        self.layer.cornerRadius = 8
+        self.backgroundColor = UIColor.AppColors.darkGray
+        self.setTitle(title, for: .normal)
+    }
 }
