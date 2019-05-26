4b56b8adc6d540c7cad815dd51d2c2f66f23d2a8
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 09:52:35 -0300

    added newdebtVC file

---
+++ Dox/Views/NewDebtVC/NewDebtViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewDebtViewController.swift
+//  Dox
+//
+//  Created by Bia Plutarco on 26/05/19.
+//  Copyright © 2019 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class NewDebtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+    }
+}
