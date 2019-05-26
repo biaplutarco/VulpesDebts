29771a8458487fbeb8d8f0879121febd00e2e662
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 18:43:26 -0300

    selection style cell = .none

--- Dox/Views/Debts/Components/DebtCell.swift
+++ Dox/Views/Debts/Components/DebtCell.swift
@@ -67,6 +67,7 @@ class DebtCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        self.selectionStyle = .none
     }
     
     func setupCell() {
