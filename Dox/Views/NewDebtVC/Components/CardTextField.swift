fb287984084dd74035174fda4c951b2c7b748c8e
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-26 12:41:22 -0300

    added textfiled class

--- Dox/Views/NewDebtVC/Components/CardTextField.swift
+++ Dox/Views/NewDebtVC/Components/CardTextField.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class CardTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    convenience init(backgroundColor color: UIColor) {
+        self.init(frame: CGRect.zero)
+        configTextField(backgroundColor: color)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension CardTextField: ShadowProtocol {
+    private func configTextField(backgroundColor: UIColor) {
+        self.backgroundColor = backgroundColor
+        self.layer.cornerRadius = 8
+        self.configShadowIn(view: self, isDark: false)
+    }
 }
